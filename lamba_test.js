4b39c825c8d67167927ff3e45a8c95c3decc3609
doritoes <39832079+doritoes@users.noreply.github.com>
2024-02-26 14:48:55 -0500

    Update lamba_test.js

--- lamba_test.js
+++ lamba_test.js
@@ -1,22 +1,14 @@
-exports.handler = async function (event, context) {
-    if (!event) {
-        return {
-            statusCode: 400, // HTTP Bad Request
-            body: "Error: Received nil event",
-            headers: { "Content-Type": "text/plain" } 
-        };
-    }
-
-    let clientIp;
-    if (event.headers['X-Forwarded-For']) {
-        clientIp = event.headers['X-Forwarded-For'].split(",")[0];
-    } else if (event.requestContext.identity.sourceIp) {
-        clientIp = event.requestContext.identity.sourceIp;
-    } else {
-        clientIp = "IP Address Not Found";
-    }
-
-    const htmlResponse = `
+export const handler = async (event) => {
+  const sourceIp = event.sourceIp;
+  let clientIp;
+  if (event.xForwardedFor && event.xForwardedFor !== "") {
+      clientIp = event.xForwardedFor.split(",")[0];
+  } else if(event.sourceIp && event.sourceIp !== "") {
+      clientIp = event.sourceIp;
+  } else {
+      clientIp = "IP Address Not Found";
+  }
+const htmlResponse = `
     <html>
     <body>
         <h1>Your IP Address</h1>
@@ -24,13 +16,10 @@ exports.handler = async function (event, context) {
     </body>
     </html>
     `;
-
-    console.log("IP Address:", event.requestContext.identity.sourceIp);
-    console.log("X-Forwarded-For:", event.headers['X-Forwarded-For']);
-
-    return {
-        statusCode: 200, // HTTP OK
-        body: htmlResponse,
-        headers: { "Content-Type": "text/html" } 
-    };
+  const response = {
+    statusCode: 200,
+    body: htmlResponse,
+    headers: { "Content-Type": "text/html" } 
+  };
+  return response;
 };
