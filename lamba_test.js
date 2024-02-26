16dd4ab77cf45cc104e49a19447c6c7eb425ec40
doritoes <39832079+doritoes@users.noreply.github.com>
2024-02-26 14:12:13 -0500

    Update lamba_test.js

--- lamba_test.js
+++ lamba_test.js
@@ -1,30 +1,38 @@
+const AWS = require('aws-sdk'); // Import the AWS SDK 
+
 exports.handler = async function (event, context) {
-  let client_ip;
+    if (!event) {
+        return {
+            statusCode: 400, // HTTP Bad Request
+            body: "Error: Received nil event",
+            headers: { "Content-Type": "text/plain" } 
+        };
+    }
 
-  // Prioritize 'X-Forwarded-For', handling potential multiple IPs
-  if (event.headers['X-Forwarded-For']) {
-    client_ip = event.headers['X-Forwarded-For'].split(',')[0];
-  } else if (event.requestContext && event.requestContext.identity.sourceIp) { 
-    // Fallback to API Gateway's 'sourceIp'
-    client_ip = event.requestContext.identity.sourceIp;
-  } else {
-    client_ip = "IP Address Not Found";
-  }
+    let clientIp;
+    if (event.headers['X-Forwarded-For']) {
+        clientIp = event.headers['X-Forwarded-For'].split(",")[0];
+    } else if (event.requestContext.identity.sourceIp) {
+        clientIp = event.requestContext.identity.sourceIp;
+    } else {
+        clientIp = "IP Address Not Found";
+    }
 
-  const html_response = `
+    const htmlResponse = `
     <html>
     <body>
         <h1>Your IP Address</h1>
-        <p>${client_ip}</p>
+        <p>${clientIp}</p>
     </body>
     </html>
-  `;
+    `;
 
-  return {
-    statusCode: 200,
-    body: html_response,
-    headers: {
-      'Content-Type': 'text/html'
-    }
-  };
+    console.log("IP Address:", event.requestContext.identity.sourceIp);
+    console.log("X-Forwarded-For:", event.headers['X-Forwarded-For']);
+
+    return {
+        statusCode: 200, // HTTP OK
+        body: htmlResponse,
+        headers: { "Content-Type": "text/html" } 
+    };
 };
