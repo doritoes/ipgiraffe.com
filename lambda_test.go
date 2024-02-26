f49c403dd5e8b86dd6cfc7547f192cf53d7b120b
doritoes <39832079+doritoes@users.noreply.github.com>
2024-02-26 13:43:05 -0500

    Update lambda_test.go

--- lambda_test.go
+++ lambda_test.go
@@ -1,35 +1,37 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
-    "strings"
-    "github.com/aws/aws-lambda-go/events"
-    "github.com/aws/aws-lambda-go/lambda"
+	"context"
+	"fmt"
+	"net/http"
+	"strings"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
 )
 
-func handler(event events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-    clientIp := "IP Address Not Found"
-    
-    if event.Body != "" {
-        var eventData map[string]string
-        err := json.Unmarshal([]byte(event.Body), &eventData) // Unmarshal JSON event body
-        if err == nil {
-            if sourceIp, ok := eventData["sourceIp"]; ok { // Check if key exists
-                clientIp = sourceIp
-            }
-            if xForwardedFor, ok := eventData["xForwardedFor"]; ok { // Check if key exists
-                clientIp = strings.Split(xForwardedFor, ",")[0]
-            }
+type MyEvent struct {
+	SourceIP   string `json:"sourceIp"`
+	XForwarded string `json:"xForwardedFor"`
+}
 
-        }
-    }
-    // Prioritize X-Forwarded-For
-    if xForwardedFor := event.Headers["x-Forwarded-For"]; xForwardedFor != "" {
-        clientIp = strings.Split(xForwardedFor, ",")[0]
-    }
-
-    htmlResponse := fmt.Sprintf(`
+func handler(ctx context.Context, event *MyEvent) (events.APIGatewayProxyResponse, error) {
+	var clientIp string
+	if event == nil {
+		return events.APIGatewayProxyResponse{
+			StatusCode: http.StatusBadRequest, // Or another appropriate error code
+			Body:       "Error: Received nil event",
+			Headers:    map[string]string{"Content-Type": "text/plain"}, // Adjust if needed
+		}, fmt.Errorf("received nil event")
+	}
+	if event.XForwarded != "" {
+		clientIp = strings.Split(event.XForwarded, ",")[0]
+	} else if event.SourceIP != "" {
+		clientIp = event.SourceIP
+	} else {
+		clientIp = "IP Address Not Found"
+	}
+	htmlResponse := fmt.Sprintf(`
         <html>
         <body>
             <h1>Your IP Address</h1>
@@ -37,14 +39,15 @@ func handler(event events.APIGatewayProxyRequest) (events.APIGatewayProxyRespons
         </body>
         </html>
     `, clientIp)
-
-    return events.APIGatewayProxyResponse{
-        StatusCode: http.StatusOK,
-        Body:       htmlResponse,
-        Headers:    map[string]string{"Content-Type": "text/html"},
-    }, nil
+	fmt.Println("IP Address:", event.SourceIP)
+	fmt.Println("Xforwarded:", event.XForwarded)
+	return events.APIGatewayProxyResponse{
+		StatusCode: http.StatusOK,
+		Body:       htmlResponse,
+		Headers:    map[string]string{"Content-Type": "text/html"},
+	}, nil
 }
 
 func main() {
-    lambda.Start(handler)
+	lambda.Start(handler)
 }
