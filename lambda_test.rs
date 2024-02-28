8bd9d3c6a78eb4ac95b1989f2118aa9e1fe3f68e
doritoes <39832079+doritoes@users.noreply.github.com>
2024-02-28 18:14:31 -0500

    Update lambda_test.rs

--- lambda_test.rs
+++ lambda_test.rs
@@ -1,10 +1,10 @@
 use lambda_runtime::{service_fn, Error};
-use serde::{Deserialize, Serialize};
 use aws_lambda_events::event::apigw::ApiGatewayProxyRequest;
+use serde::{Deserialize, Serialize};
 
 #[derive(Deserialize, Serialize)]
 struct MyEvent {
-    source_ip: Option<String>,     // Make fields optional 
+    source_ip: Option<String>,
     x_forwarded_for: Option<String>,
 }
 
@@ -12,7 +12,7 @@ struct MyEvent {
 struct MyResponse {
     status_code: u16,
     body: String,
-    headers: std::collections::HashMap<String, String>, 
+    headers: std::collections::HashMap<String, String>,
 }
 
 #[tokio::main]
@@ -22,30 +22,7 @@ async fn main() -> Result<(), Error> {
     Ok(())
 }
 
-async fn handler(e: ApiGatewayProxyRequest) -> Result<MyResponse, Error> {
-    let event: MyEvent = serde_json::from_str(&e.body.unwrap_or_default())?; // Handle potential parse errors
-
-    let client_ip = match (event.x_forwarded_for, event.source_ip) {
-        (Some(x_forwarded), _) => x_forwarded.split(',').next().unwrap_or("IP Address Not Found").to_string(),
-        (_, Some(source_ip)) => source_ip,
-        _ => "IP Address Not Found".to_string(), 
-    }; 
-
-    let html_response = format!(
-        r#"
-        <html>
-        <body>
-            <h1>Your IP Address</h1>
-            <p>{}</p>
-        </body>
-        </html>
-        "#,
-        client_ip
-    );
-
-    Ok(MyResponse {
-        status_code: 200,
-        body: html_response,
-        headers: [("Content-Type".to_string(), "text/html".to_string())].into_iter().collect(),
-    }) 
+async fn handler(event: LambdaEvent<ApiGatewayProxyRequest>) -> Result<MyResponse, Error> {
+    // TODO: Implement event extraction, IP logic, and response building
+    unimplemented!()
 }
