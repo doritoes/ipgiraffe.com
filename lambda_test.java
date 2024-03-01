bee36371658ae668f7aa807bbff1f711663308cc
doritoes <39832079+doritoes@users.noreply.github.com>
2024-03-01 11:36:44 -0500

    Update lambda_test.java

--- lambda_test.java
+++ lambda_test.java
@@ -13,24 +13,40 @@ public class MyRequestHandler implements RequestHandler<Object, APIGatewayProxyR
 
     @Override
     public APIGatewayProxyResponseEvent handleRequest(Object input, Context context) {
-
-        ObjectMapper objectMapper = new ObjectMapper(); 
+        ObjectMapper objectMapper = new ObjectMapper();
         APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent();
         response.setStatusCode(200);
 
         Map<String, String> headers = new HashMap<>();
-        headers.put("Content-Type", "application/json");
+        headers.put("Content-Type", "text/html"); // Set Content-Type to 'text/html'
         response.setHeaders(headers);
 
         try {
-            // Cast the input to a Map (since it's direct JSON input in a test)
             Map inputMap = objectMapper.convertValue(input, Map.class);
 
-            String value = (String) inputMap.get("foo"); 
+            String value = null; 
 
-            response.setBody("{\"body\": \"" + value + "\"}");
+            if (inputMap.containsKey("xForwardedFor")) {
+                String values = (String) inputMap.get("xForwardedFor");
+                value = values.split(",")[0]; 
+            } else if (inputMap.containsKey("sourceIp")) {
+                value = (String) inputMap.get("sourceIp");
+            } else {
+                value = "No IP found"; 
+            }
+
+            // Build HTML response
+            String htmlResponse = 
+                "<html>" +
+                "<body>" +
+                "<h1>Your IP Address</h1>" +
+                "<p>" + value + "</p>" +
+                "</body>" +
+                "</html>";
+
+            response.setBody(htmlResponse);
+
         } catch (Exception e) {
-            // Handle error with more descriptive message
             response.setBody("{\"error\": \"Error processing input: " + e.getMessage() + "\"}");
             response.setStatusCode(500);
         }
