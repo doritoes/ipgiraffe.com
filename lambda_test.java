425190f8a582f5d9bf302cd826c62bd712bbaa12
doritoes <39832079+doritoes@users.noreply.github.com>
2024-03-03 11:00:36 -0500

    Update lambda_test.java

--- lambda_test.java
+++ lambda_test.java
@@ -9,17 +9,19 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class MyRequestHandler implements RequestHandler<Map<String, Object>, Map<String, Object>> { 
-
     @Override
     public Map<String, Object> handleRequest(Map<String, Object> event, Context context) {
         String inputString = event.containsKey("xForwardedFor") ? ((String) event.get("xForwardedFor")).split(",")[0] :
                              event.containsKey("sourceIp") ? (String) event.get("sourceIp") : "IP Address Not Found";
-
-        // Create response body
         Map<String, Object> responseBody = new HashMap<>();
-		String outputString = "<html><body><h1>Your IP Address</h1><p>" + inputString +  "</p></body></html>";
+        String outputString = 
+            "<html>" +
+            "<body>" + 
+            "<h1>Your IP Address</h1>" + 
+            "<p>" + inputString + "</p>" + 
+            "</body>" +
+            "</html>";
         responseBody.put("body", outputString);
-
         return responseBody; 
     }
 }
